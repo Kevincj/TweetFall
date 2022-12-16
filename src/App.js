84f5bbfc651f1e938f13a97b63d4f3d450109784
Kevin Jiang <kcj1017@gmail.com>
2022-12-16 15:33:00 -0500

    Initial commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Hello(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
 function App() {
+  fetch("http://localhost:3001/").then(res => res.json()).then(data => console.log(data));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello world="World" />
     </div>
   );
 }
