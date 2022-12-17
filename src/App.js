647abd5168a72211f662a271a680b542ee52f5fd
Kevin Jiang <kcj1017@gmail.com>
2022-12-16 21:40:30 -0500

    Reinitializes project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import React from 'react';
+import { ReactPhotoCollage } from "react-photo-collage";
 import './App.css';
 import { useEffect } from 'react';
 
@@ -6,14 +8,27 @@ function Hello(props) {
   return <h1>Hello, {props.name}</h1>;
 }
 
-
+const setting = {
+  width: '600px',
+  height: ['250px', '170px'],
+  layout: [1, 4],
+  photos: [
+    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
+    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
+    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
+    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
+    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
+    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
+  ],
+  showNumOfRemainingPhotos: true
+};
 
 function App() {
   useEffect(()=>{fetch("http://localhost:3001/").then(res => res.json()).then(data => console.log(data));}, []);
 
   return (
     <div>
-      <Hello world="World" />
+      <ReactPhotoCollage {...setting}/>
     </div>
   );
 }
