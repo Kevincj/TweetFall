e3bd2844a091ad871a76840e5d28bed09a0ff757
Kevin Jiang <kcj1017@gmail.com>
2022-12-17 15:42:17 -0500

    Adds packages.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
-import logo from './logo.svg';
+
 import React from 'react';
-import { ReactPhotoCollage } from "react-photo-collage";
+// import { ReactPhotoCollage } from "react-photo-collage";
 import './App.css';
 import { useEffect } from 'react';
+import configData from "./config.json";
 
-function Hello(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
 
-const setting = {
-  width: '600px',
-  height: ['250px', '170px'],
-  layout: [1, 4],
-  photos: [
-    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
-    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
-    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
-    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
-    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
-    { source: 'https://pbs.twimg.com/media/FkGrxP6XgAAxTbm?format=jpg&name=large' },
-  ],
-  showNumOfRemainingPhotos: true
-};
 
 function App() {
-  useEffect(()=>{fetch("http://localhost:3001/").then(res => res.json()).then(data => console.log(data));}, []);
+  useEffect(()=>{fetch(configData.SERVER_URL).then(res => res.json()).then(res=>res.data).then(data => console.log(data.flatMap(d=> d.media.map(child => Object.assign({username: d.username}, child)))));}, []);
 
   return (
     <div>
-      <ReactPhotoCollage {...setting}/>
+      {/* <ReactPhotoCollage {...setting}/> */}
     </div>
   );
 }
